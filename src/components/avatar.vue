dad477842f3dd9c4a6ebb3aec0ad655f41fe4b7d
romamchur <romamchur@gmail.com>
2018-12-01 19:04:12 +0200

    todo

---
+++ src/components/avatar.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-menu
+            transition="slide-y-transition"
+            bottom
+    >
+        <div class="td-avatar"  slot="activator">
+            <v-avatar size="36px">
+                <img src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460" alt="Avatar">
+            </v-avatar>
+
+            <span class="td-avatar__name">Roman Mamchur</span>
+        </div>
+        <v-list>
+            <v-list-tile>
+                <v-list-tile-title>Log out</v-list-tile-title>
+            </v-list-tile>
+        </v-list>
+    </v-menu>
+
+</template>
+<script>
+    export default {
+    }
+</script>
+<style scoped lang="less">
+    .td-avatar {
+        display: flex;
+        align-items: center;
+
+        &__name {
+            margin-left: 8px;
+        }
+    }
+</style>
---
+++ src/store/modules/auth.js
@@ -0,0 +1,5 @@
+const auth = {
+    state: {}
+}
+
+export default auth;
